d54540709e7bb99d2477ce1757d8188a0b9848cd
Anthony Bendeler <anthonybendeler@gmail.com>
2020-12-12 12:30:04 +0100

    feature: sync footer styling with header and main content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,5 +2,7 @@ import * as React from "react";
 import styles from "./footer.module.scss";
 
 export const Footer: React.FC = () => (
-  <footer className={styles.root}>footer</footer>
+  <footer className={styles.root}>
+    <div className={styles.content}>footer</div>
+  </footer>
 );
